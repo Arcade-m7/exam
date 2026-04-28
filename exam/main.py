28edefc811c3ea884370f261c4a0a91d61aff186
CHARKAOUI Haitam <arcade.m7.2007@gmail.com>
2026-04-28 12:12:29 +0100

    update

--- exam/main.py
+++ exam/main.py
@@ -1,5 +1,11 @@
-from utils import PathEntry, SearchEntry, algorithmes
+from src import PathEntry, algorithmes, Search
 from os import getcwd,scandir
+from time import time,sleep
 
-for searchresult in SearchEntry.Search('c:/','*.txt'):
-    print(searchresult)
+def CountTimeToExecAFunction(func,*args,**keys):
+    st = time() ; return func(*args,**keys), time() - st
+
+icons = 'https://www.flaticon.com/packs/file-extension-4?word=files%20and%20folders'
+"https://www.flaticon.com/packs/files-color/2"
+"https://www.flaticon.com/packs/files-and-folders-25?word=files%20and%20folders"
+
